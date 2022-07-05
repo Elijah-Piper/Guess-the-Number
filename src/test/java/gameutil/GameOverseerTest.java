c45086991d73663b8b47ad006f088b409e469543
Elijah-Piper <piperelijah@gmail.com>
2022-07-05 12:57:14 -0700

    Added unit testing for GameOverseer class.

--- src/test/java/gameutil/GameOverseerTest.java
+++ src/test/java/gameutil/GameOverseerTest.java
@@ -3,10 +3,42 @@ package gameutil;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 class GameOverseerTest {
 
     @Test
-    void assertTrueDummyTest() {
-        assertTrue(true);
+    void scanForPlayerNameReturnsName() {
+        String testNameStr = "Java-Aficionado";
+        InputStream testName = new ByteArrayInputStream(testNameStr.getBytes());
+
+        System.setIn(testName);
+        GameOverseer game = new GameOverseer();
+
+        assertEquals(testNameStr, game.getPlayerName());
+    }
+
+    @Test
+    void scanForGuessReturnsGuess() {
+        System.setIn(new ByteArrayInputStream("NAME".getBytes()));
+        GameOverseer game = new GameOverseer();
+
+        String testGuessStr = "10";
+        InputStream testGuess = new ByteArrayInputStream(testGuessStr.getBytes());
+
+        System.setIn(testGuess);
+
+        assertEquals(10, game.scanForGuess());
+    }
+
+    @Test
+    void numberIsBetweenOneAndTwenty() {
+        for (int i = 0; i < 160; i++) {
+            System.setIn(new ByteArrayInputStream("NAME".getBytes()));
+            GameOverseer game = new GameOverseer();
+
+            if (game.getNumber() < 1 || game.getNumber() > 20) fail("Number out of bounds");
+        }
     }
 }
